078d7b4620a239beb36f5511c85fbcc24e426e59
Neelzzboi <85091341+Neelzzboi@users.noreply.github.com>
2023-02-11 20:09:04 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -9,5 +9,30 @@ function start(){
 
 recognition.onresult = function(event){
     console.log(event);
+    var content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = content;
+    console.log(content);
+    speak();
 }
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+});
+
+
+
+
+
 
